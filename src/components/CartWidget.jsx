5e522fcadcd88f57c8cf97e67654dd8655ea7d21
Thomas <thomandoclases@gmail.com>
2024-07-24 12:26:25 -0300

    primer comit

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,9 @@
+import Cart from "../assets/cart.png";
+
+export const CartWidget = () => {
+  return (
+    <>
+      <img src={Cart} alt="" height={24}/><span>3</span>
+    </>
+  );
+};
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+import { CartWidget } from "./CartWidget";
+
+export const NavBar = () => {
+  return (
+    <>
+      <Navbar bg="light" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="#home">Hali Tienda</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#Tienda">Tienda</Nav.Link>
+            <Nav.Link href="#Nosotros">Nosotros</Nav.Link>
+            <Nav.Link href="#contacto">contacto</Nav.Link>
+          </Nav>
+            <CartWidget />
+        </Container>
+      </Navbar>
+    </>
+  );
+};
