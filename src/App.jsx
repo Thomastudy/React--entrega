57eb80b5fa06d3fe030248c3d919f0660d9a962d
Thomas <thomandoclases@gmail.com>
2025-04-14 12:39:01 -0300

    testeando fecha

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import { AdminPage } from "./components/AdminPage";
 function App() {
   return (
     <>
+    {/* testeando fecha */}
       <Provider>
         <BrowserRouter>
           {location.pathname !== "/haliadmin" && <Header />}
